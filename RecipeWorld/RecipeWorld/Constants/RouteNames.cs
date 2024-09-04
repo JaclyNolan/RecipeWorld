6e1ad8258c02e964ac00e17ab3208de59914d340
Jack Ánh <94823763+JaclyNolan@users.noreply.github.com>
2024-09-04 11:26:33 +0700

    Add RouteNames, base Add, Edit pages and Delete Recipe feature

---
+++ RecipeWorld/RecipeWorld/Constants/RouteNames.cs
@@ -0,0 +1,23 @@
+﻿using RecipeWorld.Components.Pages.RecipePages;
+
+namespace RecipeWorld.Constants
+{
+    public static class RouteNames
+    {
+        public const string Login = "/login";
+        public const string Index = "/";
+        public const string Unauthorized = "/unauthorized";
+        public const string Forbidden = "/forbidden";
+        public const string NotFound = "/404";
+        public const string ServerError = "/server-error";
+
+        public static class Recipe
+        {
+            public const string List = "/recipes";
+            public const string Create = "/recipes/create";
+            public const string Edit = "/recipes/edit/{" + nameof(RecipeEdit.RecipeId) + "}";
+            public static string GetEditRoute(string? recipeId) => !string.IsNullOrEmpty(recipeId) ? $"/recipes/edit/{recipeId}" : "/recipes/edit";
+        }
+    }
+
+}
